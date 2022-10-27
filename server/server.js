6b17778fbe4626b9826243f8ba9a80abd5be5412
Canh <pcanhgm@gmail.com>
2022-10-27 14:56:00 +0700

    server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express") 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("connected");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server listening the port http://localhost/" + port);
+});
